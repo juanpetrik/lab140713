8c6af7fadd0247dc75d6ba46c2fa1dd7b89a0131
JuanPetrik <juanpablopetrik@gmail.com>
2016-08-11 21:42:51 -0300

    #2 - Criado Servlets Venda e Entrega.

---
+++ lab140713/src/main/java/br/petrik/servlets/Venda.java
@@ -0,0 +1,21 @@
+package br.petrik.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(value = "/Venda", asyncSupported = true)
+public class Venda extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+
+}
