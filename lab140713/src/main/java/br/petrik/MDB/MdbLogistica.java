5daab630127deee214606da4c390e32e2cc70724
JuanPetrik <juanpablopetrik@gmail.com>
2016-08-14 00:50:32 -0300

    #7 - Criação do MDBLogistica e também implementação do mesmo.

---
+++ lab140713/src/main/java/br/petrik/MDB/MdbLogistica.java
@@ -0,0 +1,45 @@
+package br.petrik.MDB;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import br.petrik.servlets.Venda;
+
+@MessageDriven(name = "MdbLogistica", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TopicVenda"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class MdbLogistica implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbLogistica.class.toString());
+
+	@Override
+	public void onMessage(Message rcvMessage) {
+		ObjectMessage msg = null;
+		try {
+			if (rcvMessage instanceof ObjectMessage) {
+				msg = (ObjectMessage) rcvMessage;
+				Venda venda = (Venda) msg.getObject();
+
+				LOGGER.info("Mensagem : " + msg);
+				LOGGER.info("Processando");
+
+				Thread.sleep(30000);
+
+				LOGGER.info("A entrega foi mandada embora kk..");
+			} else {
+				LOGGER.warning("Message of Wrong type: " + rcvMessage);
+			}
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
